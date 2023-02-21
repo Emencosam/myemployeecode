3783dc9c95f45923b63fcf20049c5474b08e8fe1
Emencosam <emenco04@gmail.com>
2023-02-21 01:11:13 -0600

    first commit

--- main.tf
+++ main.tf
@@ -3,20 +3,6 @@ provider "aws" {
   access_key = "AKIAS6CWZEDAHPTKUTHL"
   secret_key = "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"
 }
-
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the GitHub Provider
-provider "github" {
-  token = "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"
-} 
  
 
 
